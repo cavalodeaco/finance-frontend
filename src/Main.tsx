df65f15d525d5be698e257e5856d8c2b5a4f9c2b
Gian Fritsche <gian.fritsche@gmail.com>
2023-09-17 20:56:30 -0300

    feat(main): app shell with responsive navbar and header

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,56 @@
-import { AppShell } from "@mantine/core";
+import {
+  AppShell,
+  Burger,
+  Header,
+  MediaQuery,
+  Title,
+  createStyles,
+  rem,
+  useMantineTheme
+} from "@mantine/core";
+import { useState } from "react";
 import { NavbarNested } from "./NavbarNested";
 
+const useStyles = createStyles((theme) => ({
+  header: {
+    padding: theme.spacing.md,
+    paddingTop: 0,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    borderBottom: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
+    paddingBottom: 0,
+  },
+}));
+
 export default function Main() {
+  const [openedMenu, setOpenedMenu] = useState(false);
+  const theme = useMantineTheme();
+  const { classes } = useStyles();
   return (
     <AppShell
       padding="md"
-      navbar={<NavbarNested />}
+      navbar={<NavbarNested openedMenu={openedMenu} />}
+      header={
+        <Header height={60} className={classes.header}>
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+              <Burger
+                opened={openedMenu}
+                onClick={() => setOpenedMenu((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+            <Title order={2}>🏦 Finance</Title>
+          </div>
+        </Header>
+      }
       styles={(theme) => ({
         main: {
           backgroundColor:
--- src/NavbarNested.tsx
+++ src/NavbarNested.tsx
@@ -1,68 +1,31 @@
-import { Navbar, Group, Code, ScrollArea, createStyles, rem } from '@mantine/core';
-import {
-  IconNotes,
-  IconCalendarStats,
-  IconGauge,
-  IconPresentationAnalytics,
-  IconFileAnalytics,
-  IconAdjustments,
-  IconLock,
-} from '@tabler/icons-react';
-import { UserButton } from './UserButton';
-import { LinksGroup } from './NavbarLinksGroup';
-import { Logo } from './Logo';
+import { Navbar, ScrollArea, createStyles, rem } from "@mantine/core";
+import { IconTablePlus } from "@tabler/icons-react";
+import { LinksGroup } from "./NavbarLinksGroup";
+import { UserButton } from "./UserButton";
 
-const mockdata = [
-  { label: 'Dashboard', icon: IconGauge },
-  {
-    label: 'Market news',
-    icon: IconNotes,
-    initiallyOpened: true,
-    links: [
-      { label: 'Overview', link: '/' },
-      { label: 'Forecasts', link: '/' },
-      { label: 'Outlook', link: '/' },
-      { label: 'Real time', link: '/' },
-    ],
-  },
-  {
-    label: 'Releases',
-    icon: IconCalendarStats,
-    links: [
-      { label: 'Upcoming releases', link: '/' },
-      { label: 'Previous releases', link: '/' },
-      { label: 'Releases schedule', link: '/' },
-    ],
-  },
-  { label: 'Analytics', icon: IconPresentationAnalytics },
-  { label: 'Contracts', icon: IconFileAnalytics },
-  { label: 'Settings', icon: IconAdjustments },
-  {
-    label: 'Security',
-    icon: IconLock,
-    links: [
-      { label: 'Enable 2FA', link: '/' },
-      { label: 'Change password', link: '/' },
-      { label: 'Recovery codes', link: '/' },
-    ],
-  },
+const data = [
+  { label: "New Record", icon: IconTablePlus },
+  // { // example of nested links
+  //   label: 'Market news',
+  //   icon: IconNotes,
+  //   initiallyOpened: true,
+  //   links: [
+  //     { label: 'Overview', link: '/' },
+  //     { label: 'Forecasts', link: '/' },
+  //     { label: 'Outlook', link: '/' },
+  //     { label: 'Real time', link: '/' },
+  //   ],
+  // },
 ];
 
 const useStyles = createStyles((theme) => ({
   navbar: {
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
-    paddingBottom: 0,
-  },
-
-  header: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
     padding: theme.spacing.md,
-    paddingTop: 0,
-    marginLeft: `calc(${theme.spacing.md} * -1)`,
-    marginRight: `calc(${theme.spacing.md} * -1)`,
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    borderBottom: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
-    }`,
+    paddingBottom: /iPad|iPhone|iPod/.test(navigator.userAgent)
+      ? `calc(${theme.spacing.xl} * 2 + 60px)`
+      : 0,
   },
 
   links: {
@@ -70,35 +33,29 @@ const useStyles = createStyles((theme) => ({
     marginRight: `calc(${theme.spacing.md} * -1)`,
   },
 
-  linksInner: {
-    paddingTop: theme.spacing.xl,
-    paddingBottom: theme.spacing.xl,
-  },
-
   footer: {
     marginLeft: `calc(${theme.spacing.md} * -1)`,
     marginRight: `calc(${theme.spacing.md} * -1)`,
     borderTop: `${rem(1)} solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[3]
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
     }`,
   },
 }));
 
-export function NavbarNested() {
+export function NavbarNested({ openedMenu }: { openedMenu: boolean }) {
   const { classes } = useStyles();
-  const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
+  const links = data.map((item) => <LinksGroup {...item} key={item.label} />);
 
   return (
-    <Navbar height={"100vh"} width={{ sm: 300 }} p="md" className={classes.navbar}>
-      <Navbar.Section className={classes.header}>
-        <Group position="apart">
-          <Logo width={rem(120)} />
-          <Code sx={{ fontWeight: 700 }}>v3.1.2</Code>
-        </Group>
-      </Navbar.Section>
-
+    <Navbar
+      hiddenBreakpoint="sm"
+      hidden={!openedMenu}
+      width={{ base: 300 }}
+      height={`calc(100vh - 60px)`}
+      className={classes.navbar}
+    >
       <Navbar.Section grow className={classes.links} component={ScrollArea}>
-        <div className={classes.linksInner}>{links}</div>
+        <div>{links}</div>
       </Navbar.Section>
 
       <Navbar.Section className={classes.footer}>
