1ffc40849b84852148607c217e043800d7685434
Gian Fritsche <gian.fritsche@gmail.com>
2023-09-17 21:18:48 -0300

    feat(logout): add logout button to clear localStorage and reload page

--- src/UserButton.tsx
+++ src/UserButton.tsx
@@ -5,18 +5,54 @@ import {
   Avatar,
   Text,
   createStyles,
-} from '@mantine/core';
-import { IconChevronRight } from '@tabler/icons-react';
+  Collapse,
+  Button,
+  rem,
+} from "@mantine/core";
+import {
+  IconChevronLeft,
+  IconChevronRight,
+  IconLogout,
+} from "@tabler/icons-react";
+import { useState } from "react";
 
 const useStyles = createStyles((theme) => ({
   user: {
-    display: 'block',
-    width: '100%',
+    display: "block",
+    width: "100%",
     padding: theme.spacing.md,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
 
-    '&:hover': {
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[8]
+          : theme.colors.gray[0],
+    },
+  },
+
+  chevron: {
+    transition: "transform 200ms ease",
+  },
+
+  link: {
+    fontWeight: 500,
+    display: "block",
+    textDecoration: "none",
+    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+    marginTop: theme.spacing.md,
+    fontSize: theme.fontSizes.sm,
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+
+    "&:hover": {
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[7]
+          : theme.colors.gray[0],
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
     },
   },
 }));
@@ -28,11 +64,29 @@ interface UserButtonProps extends UnstyledButtonProps {
   icon?: React.ReactNode;
 }
 
-export function UserButton({ image, name, email, icon, ...others }: UserButtonProps) {
-  const { classes } = useStyles();
+export function UserButton({
+  image,
+  name,
+  email,
+  icon,
+  ...others
+}: UserButtonProps) {
+  const { classes, theme } = useStyles();
+
+  const [opened, setOpened] = useState(false);
+  const ChevronIcon = theme.dir === "ltr" ? IconChevronRight : IconChevronLeft;
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.reload();
+  }
 
   return (
-    <UnstyledButton className={classes.user} {...others}>
+    <UnstyledButton
+      className={classes.user}
+      {...others}
+      onClick={() => setOpened((opened) => !opened)}
+    >
       <Group>
         <Avatar src={image} radius="xl" />
 
@@ -46,8 +100,27 @@ export function UserButton({ image, name, email, icon, ...others }: UserButtonPr
           </Text>
         </div>
 
-        {icon || <IconChevronRight size="0.9rem" stroke={1.5} />}
+        <ChevronIcon
+          className={classes.chevron}
+          size="1rem"
+          stroke={1.5}
+          style={{
+            transform: opened
+              ? `rotate(${theme.dir === "rtl" ? -90 : 90}deg)`
+              : "none",
+          }}
+        />
       </Group>
+      <Collapse in={opened}>
+        <Text<"a">
+          component="a"
+          className={classes.link}
+          onClick={logout}
+        >
+          <IconLogout size="0.9rem" stroke={1.5} />
+          &nbsp;Logout
+        </Text>
+      </Collapse>
     </UnstyledButton>
   );
 }
